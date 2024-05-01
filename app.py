a58a0beed202fe3e2c6efba061874da921cdbe92
Jayesh Pandey <100137975+jayesh-RN@users.noreply.github.com>
2024-05-02 00:09:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 import numpy as np
 import pandas as pd
-from PIL import Image
+#from PIL import Image
 import streamlit as st
 import pickle
 
 lg = pickle.load(open('placement.pkl','rb'))
 
 # web app
-img = Image.open('Job-Placement-Agency.jpg')
-st.image(img,width=650)
+#img = Image.open('Job-Placement-Agency.jpg')
+#st.image(img,width=650)
 st.title("Job Placement Prediciton Model")
 
 input_text = st.text_input("Enter all features")
